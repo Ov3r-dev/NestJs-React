1a1e5c1fb2951584630b5aeb43ec6d0f41c0e575
unknown <joaopedrocontabil@hotmail.com>
2025-06-09 17:20:15 -0300

    Arrumado.

--- backend/src/fornecedor_data/dto/update-fornecedor_datum.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFornecedorDatumDto } from './create-fornecedor_datum.dto';
-
-export class UpdateFornecedorDatumDto extends PartialType(CreateFornecedorDatumDto) {}
--- backend/src/fornecedor_data/fornecedor_data.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FornecedorDataService } from './fornecedor_data.service';
-import { FornecedorDataController } from './fornecedor_data.controller';
-
-@Module({
-  controllers: [FornecedorDataController],
-  providers: [FornecedorDataService],
-})
-export class FornecedorDataModule {}
